c8fbd3061169d3cfaa8cb3f09b047a6a237959e1
paparae <jieunkim03@snu.ac.kr>
2025-04-30 07:47:21 +0000

    Deploying to gh-pages from @ paparae/paparae.github.io@511fbda183b79dbab8ff2758b24a8bad6da6c6b5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-나의-첫-jekyll-블로그-포스트",
+            },{id: "post-",
+        
+          title: "",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/2025-04-30-jongang-song/";
+          
+        },
+      },{id: "post-나의-첫-jekyll-블로그-포스트",
         
           title: "나의 첫 Jekyll 블로그 포스트",
         
