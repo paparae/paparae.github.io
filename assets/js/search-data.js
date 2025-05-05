1b14cb2078df660803b5fd38b738508abfe30bd8
paparae <jieunkim03@snu.ac.kr>
2025-05-05 12:09:14 +0000

    Deploying to gh-pages from @ paparae/paparae.github.io@264fe259fc6142faa46d50a3503bbd1cc7f381a9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,18 +72,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-",
-        
-          title: "",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/2025-04-30-jongang-song/";
-          
-        },
-      },{id: "post-markdown-문법-기초",
+            },{id: "post-markdown-문법-기초",
         
           title: "Markdown 문법 기초",
         
